aafc861736f5f107dfd0b21f2ae2a63b51dfab9b
Christopher Foo <chris.foo@gmail.com>
2013-04-14 14:47:45 -0400

    Adds export script.

---
+++ visibli_export.py
@@ -0,0 +1,19 @@
+import sqlite3
+import base64
+
+
+def main():
+    db = sqlite3.connect('visibli.db')
+
+    for row in db.execute('SELECT shortcode, url FROM visibli_hex '
+    'WHERE URL IS NOT NULL ORDER BY shortcode ASC'):
+        shortcode, url = row
+        shortcode_str = base64.b16encode(shortcode).decode().lower()
+
+        if '\r' in url or '\n' in url:
+            raise Exception('{} contains newline'.format(url))
+
+        print('{}|{}'.format(shortcode_str, url))
+
+if __name__ == '__main__':
+    main()
