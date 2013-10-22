cc448c5ba594619b5691ff37d1f2dcde92c3d1d3
Christopher Foo <chris.foo@gmail.com>
2013-10-21 22:23:28 -0400

    Add dopplr

---
+++ dopplr/README.rst
@@ -0,0 +1,19 @@
+Dopplr
+==============
+
+Scripts to grab http://www.dopplr.com . See http://archiveteam.org/index.php?title=Dopplr for more details.
+
+Requires Python 2 and Wget 1.14.
+
+Generic instructions
+++++++++++++++++++++
+
+* Create the ``warc`` and ``log`` directories.
+* See additional instructions inside each file
+* ``touch STOP`` to gracefully stop.
+
+
+Process
++++++++
+
+1. dopplr_places_grab.py
---
+++ dopplr/dopplr_places_grab.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+'''This script grabs only /places/XX'''
+import subprocess
+import time
+import urllib
+import os
+
+if __name__ == '__main__':
+    instance_time = int(time.time())
+    with open('dopplr_places.txt', 'r') as f:
+        for place in f:
+            if os.path.exists('STOP'):
+                print 'STOP'
+                break
+        
+            cur_time = int(time.time())
+            place = place.strip()
+            
+            if not place:
+                continue
+            
+            url = 'http://www.dopplr.com/place/{}'.format(place)
+            print 'Fetch', url
+            return_code = subprocess.call([
+                'wget', 
+                '-U', "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36",
+                '-o', 'log/{}-{}.log'.format(cur_time, place),
+                '--no-check-certificate', 
+                '--output-document', '/tmp/{}-wget.tmp'.format(instance_time),
+                '-e', 'robots=off',
+                '--timeout', '60', '--tries', '5', '--waitretry', '5', '--level', '25',
+                '--recursive',
+                '--no-parent',
+                '--include', '/place/{}'.format(place),
+                '--page-requisites',
+                '--random-wait', '--wait', '0.1',
+                '--warc-file', "warc/{}-{}.warc.gz".format(cur_time, place),
+                '--warc-header', 'operator: Archive Team',
+                '--verbose', 
+                url])
+            
+            if return_code not in (0, 4, 6, 8):
+                raise Exception('Wget error {}'.format(return_code))
+            
+            time.sleep(1)
