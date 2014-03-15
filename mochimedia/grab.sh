4ff1e6b159a1e2fa7f33f538076efb26fced20ff
Christopher Foo <chris.foo@gmail.com>
2014-03-15 19:43:28 -0400

    mochimedia/grab.sh: Fix up script to wpull.

--- mochimedia/grab.sh
+++ mochimedia/grab.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-wpull --input-file urls.txt --warc-file mochimedia-games --delete-after \
--e "robots=off" --no-cookies --tries inf --waitretry 600 --retry-connrefused \
---warc-header "operator: Archive Team" --timeout 60
+python3 -m wpull --input-file urls.txt --warc-file mochimedia-games \
+--delete-after \
+--no-robots --no-cookies --tries inf --waitretry 600 --retry-connrefused \
+--warc-header "operator: Archive Team" --timeout 60 --retry-dns-error \
+--database mochi.db
