9c12b02f4785a730996f82be91ca7434698e4bfc
Christopher Foo <chris.foo@gmail.com>
2014-03-16 15:47:04 -0400

    mochimedia.py: Get all the URLs and normalize them.

--- mochimedia/mochimedia.py
+++ mochimedia/mochimedia.py
@@ -8,8 +8,18 @@ def main():
 
     for game_obj in feed_obj['games']:
         for value in game_obj.values():
-            if isinstance(value, str) and value.startswith('http://') \
-            and 'mochi' in value:
+            if isinstance(value, str) \
+            and (value.startswith('http://') or value.startswith('https://')):
+
+                if 'http:// ' in value:
+                    value = value.replace('http:// ', 'http://')
+
+                if '\n' in value:
+                    value = value.split('\n', 1)[0]
+
+                value = value.strip()
+                value = value.replace(' ', '%20')
+
                 print(value)
 
 
